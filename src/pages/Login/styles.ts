d6bb3f309e3cf234e0e11b06d71b6c2fac3a482c
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-10-03 12:14:25 -0300

    feat: add global blue style on login page

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -43,7 +43,7 @@ export const RightContainer = styled.div`
         margin-top: 26px;
     }
     span {
-        color: blue;
+        color: ${ (props) => props.theme.color.blue};
         cursor: pointer;
     }
 `
