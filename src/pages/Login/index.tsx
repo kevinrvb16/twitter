fa8db0e3d3f64ed50d3f52a9c838816d0bc440bf
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-10-03 10:13:04 -0300

    feat: login page routing

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,7 @@
+const Login: React.FC = () => {
+    return (
+        <h1>Página de login</h1>
+    )
+}
+
+export default Login
--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
 import Home from './pages/Home'
+import Login from './pages/Login';
 import { GlobalStyles } from './styles/GlobalStyles';
 import ThemeProvider from './styles/ThemeProvider';
 const Routes = () => {
@@ -10,6 +12,9 @@ const Routes = () => {
             <Switch>
                 <Route exact path="/">
                     <Home />
+                </Route>
+                <Route path="/login">
+                    <Login />
                 </Route>
             </Switch>
         </ThemeProvider>
