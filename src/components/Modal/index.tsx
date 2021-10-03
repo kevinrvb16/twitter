b9e2b2f677aae93e4a627dc6c0844693e2cf2cc2
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-10-03 13:41:20 -0300

    feat: Modal Component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,10 +1,25 @@
+import React from "react";
 import { BackGround, Center } from "./styles";
 
-const Modal: React.FC = ({children}) => {
+interface IProps {
+    isOpen: boolean
+    setIsOpen: (isOpen: boolean) => void
+}
+
+const Modal: React.FC<IProps> = ({ children, isOpen, setIsOpen }) => {
+    const onClose = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (event.target === event.currentTarget) {
+            setIsOpen(false)}
+        }
+    
     return (
-        <BackGround>
-            <Center>{children}</Center>
-        </BackGround>
+        <>
+            {isOpen &&(
+                <BackGround onClick={ onClose }>
+                    <Center>{children}</Center>
+                </BackGround>
+            )}
+        </>
     )
 }
 export default Modal;
