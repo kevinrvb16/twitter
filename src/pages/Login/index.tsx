60cf6b21540778a8387ce2a21304eb83bacf9e17
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-10-03 11:54:01 -0300

    feat: login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,25 @@
+import { Button, Container, BackgroundContainer, RightContainer } from './styles'
+import Background from '../../assets/images/login-bg.png'
+import {FaTwitter} from 'react-icons/fa'
+
 const Login: React.FC = () => {
     return (
-        <h1>Página de login</h1>
+        <Container>
+            <BackgroundContainer>
+                <img src={Background} alt="Fundo do Twitter" />
+            </BackgroundContainer>
+            <RightContainer>
+                <FaTwitter color="D9D9D9" size={40}/>
+                <h1>Acontecendo agora</h1>
+                <h2>Inscreva-se no Twitter hoje mesmo.</h2>
+                <Button>Escreva-se com email</Button>
+                <p> Já tem uma conta? {' '}
+                    <span>
+                         Entre
+                    </span>
+                </p>
+            </RightContainer>
+        </Container>
     )
 }
 
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,63 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+
+    `
+
+export const BackgroundContainer = styled.div`
+img { 
+    height: 100vh;
+    object-fit: cover;
+}
+`
+export const RightContainer = styled.div`
+    margin-left: 30px;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    h1 {
+        margin-top: 60px;
+        margin-bottom: 32px;
+        font-weight: 700;
+        font-size: 64px;
+        font-style: normal;
+        line-height: 83px;
+        color: #D9D9D9;
+    }
+
+    h2 {
+        font-size: 31px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 40px;
+        color: #D9D9D9;
+        margin-bottom: 32px;
+    }
+
+    p {
+        font-size: 16px;
+        font-style: normal;
+        line-height: 19px;
+        margin-top: 26px;
+    }
+    span {
+        color: blue;
+        cursor: pointer;
+    }
+`
+
+export const Button = styled.button`
+    background-color: #fff;
+    color: #000;
+    border-radius: 70px;
+    height: 49px;
+    width: 381px;
+    border-radius: 70px;
+    padding: 14px, 92px, 14px, 92px;
+    border: 0;
+    font-size: 16px;
+    font-weight: 700;
+    cursor: pointer;
+`
