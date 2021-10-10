bedf2fb21d36340bb6e4bbb59a5a0e8eae6604a0
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-10-10 13:41:47 -0300

    feat(SearchInput): styles for focus state

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,10 +1,15 @@
 import { Input, InputContainer } from "./styles"
 import {FiSearch} from "react-icons/fi"
+import { useState } from "react"
 const SearchInput = ()=> {
+
+    const [isOnFocus, setIsOnFocus] = useState(false)
     return (
-        <InputContainer>
-            <FiSearch color="8899A6" size="17px"/>
-            <Input placeholder="Buscar no Twitter"/>
+        <InputContainer isOnFocus={isOnFocus}>
+            <FiSearch size="17px"/>
+            <Input placeholder="Buscar no Twitter"
+            onFocus={ () => setIsOnFocus(true)}
+            onBlur={ () => setIsOnFocus(false)}/>
         </InputContainer>
     )
 }
--- src/components/SearchInput/styles.ts
+++ src/components/SearchInput/styles.ts
@@ -1,14 +1,20 @@
 import styled from 'styled-components'
-
-export const InputContainer = styled.div`
+ interface IProps {
+     isOnFocus: boolean;
+ }
+export const InputContainer = styled.div<IProps>`
     width: 348px;
     background: #202327;
-    border: 1px solid #253341;
+    border: 1px solid ${props => props.isOnFocus ? props.theme.color.blue : '#253341'};
     box-sizing: border-box;
     border-radius: 40px;
     padding: 12px;
     display: flex;
     align-items: center;
+
+    svg {
+        stroke: ${props => props.isOnFocus ? props.theme.color.blue : '#8899A6'}
+    }
 `
 export const Input = styled.input`
     margin-left: 12px;
@@ -18,6 +24,7 @@ export const Input = styled.input`
     color: #FFF;
     font-size: 15px;
     line-height: 18px;
+    width: 100%;
     &::placeholder{
         color: ${props => props.theme.color.gray};
     }
