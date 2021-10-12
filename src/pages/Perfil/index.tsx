628610974de1a8422d432c5cc06398906f8c544c
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-10-12 14:26:36 -0300

    feat(Perfil): create fixed content

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,7 +1,24 @@
-import SideBar from "../../components/SideBar";
+import PageWrapper from "../../components/PageWrapper";
+import { FixedContentContainer, FixedContentTexts } from "./styles";
+import { BiArrowBack } from 'react-icons/bi'
+import { Link } from "react-router-dom";
 
 function Perfil() {    
-  return <SideBar/>
+  return <PageWrapper
+      fixedContent={
+      <FixedContentContainer>
+        <Link to='/'>
+          <BiArrowBack size='15px' />
+        </Link>
+        <FixedContentTexts>
+          <h1>Daniel Berg</h1>
+          <h2> 11 Tweets</h2>
+        </FixedContentTexts>
+      </FixedContentContainer>
+    }
+    >
+    Página de perfil
+  </PageWrapper>
 }
 
 export default Perfil;
---
+++ src/pages/Perfil/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const FixedContentContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    padding-left: 28px;
+`
+
+export const FixedContentTexts = styled.div`
+    margin-left: 23px;
+
+    h1{
+        font-weight: 700;
+        font-size: 19px;
+        line-height: 23px;
+    }
+
+    h2 {
+        font-weight: 500;
+        font-size: 12px;
+        line-height: 14px;
+        color: #6d777c;
+    }
+`
