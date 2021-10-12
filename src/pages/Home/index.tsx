bc38664e403a08bcfd0b7c6b3fd445bfdf3665bc
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-10-11 23:30:58 -0300

    feat(Home): add main components to Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,43 @@ function Home() {
   return (
     <Container>
       <SideBar/>
-      <Main/>
+      <Main fixedContent = {
+        <h1 style={{paddingTop:"15px", paddingBottom:"15px"}}>Daniel Berg</h1>
+      }>
+        <h1>Meu primeiro tweet!</h1>
+        <h1>Meu segundo tweet!</h1>
+        <h1>Meu terceiro tweet!</h1>
+        <div style={{paddingTop:"50px", paddingBottom:"50px"}}></div>
+        <h1>Meu primeiro tweet!</h1>
+        <h1>Meu segundo tweet!</h1>
+        <h1>Meu terceiro tweet!</h1>
+        <div style={{paddingTop:"50px", paddingBottom:"50px"}}></div>
+        <h1>Meu primeiro tweet!</h1>
+        <h1>Meu segundo tweet!</h1>
+        <h1>Meu terceiro tweet!</h1>
+        <div style={{paddingTop:"50px", paddingBottom:"50px"}}></div>
+        <h1>Meu primeiro tweet!</h1>
+        <h1>Meu segundo tweet!</h1>
+        <h1>Meu terceiro tweet!</h1>
+        <div style={{paddingTop:"50px", paddingBottom:"50px"}}></div>
+        <h1>Meu primeiro tweet!</h1>
+        <h1>Meu segundo tweet!</h1>
+        <h1>Meu terceiro tweet!</h1>
+        <div style={{paddingTop:"50px", paddingBottom:"50px"}}></div>
+        <h1>Meu primeiro tweet!</h1>
+        <h1>Meu segundo tweet!</h1>
+        <h1>Meu terceiro tweet!</h1>
+        <div style={{paddingTop:"50px", paddingBottom:"50px"}}></div>
+        <h1>Meu primeiro tweet!</h1>
+        <h1>Meu segundo tweet!</h1>
+        <h1>Meu terceiro tweet!</h1>
+        <div style={{paddingTop:"50px", paddingBottom:"50px"}}></div>
+        <h1>Meu primeiro tweet!</h1>
+        <h1>Meu segundo tweet!</h1>
+        <h1>Meu terceiro tweet!</h1>
+        <div style={{paddingTop:"50px", paddingBottom:"50px"}}></div>
+
+      </Main>
       <SearchInput/>
     </Container>
   )
