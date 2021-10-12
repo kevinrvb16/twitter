d2158ea1c528f4a4d96b2ab031359039e83b563d
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-10-12 12:01:32 -0300

    feat(Home): integrate feed with backend

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,60 +1,64 @@
+import { useEffect, useState } from "react";
 import Button from "../../components/Button";
 import PageWrapper from "../../components/PageWrapper";
 import Tweet from "../../components/Tweet";
 import { IAuth, useGlobalState } from "../../context/GlobalContext";
+import { apiWithAuth } from "../../services/api";
 import { TweetButton, TweetContainer, TweetInput, UserName } from "./styles";
 
-function Home() {
-  const { auth: {user} } = useGlobalState() as { auth: IAuth }
+interface ITweet {
+  id: string;
+  content: string;
+  user: {
+    id: string;
+    name: string;
+    username: string;
+    email: string;
+  };
+  created_at: string;
+}
 
+function Home() {
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+
+  const {
+    auth: { user },
+  } = useGlobalState() as { auth: IAuth };
+
+  const getFeed = async () => {
+    const { data } = await apiWithAuth.get<ITweet[]>("/feed");
+
+    setTweets(data);
+  };
+  useEffect(() => {
+    getFeed();
+  }, []);
   return (
-    <PageWrapper fixedContent={
-      <>
-        <UserName>{user.name}</UserName>
-        <TweetContainer>
-          <img
-            src={`https://robohash.org/${user.username}`}
-            alt={user.name}
-          />
-          <TweetInput placeholder="O que está acontecendo" />
-        </TweetContainer>
-        <TweetButton>
-          <Button>Tweet</Button>
-        </TweetButton>
-      </>
-    }>
-      <Tweet username={user.username} name={user.name}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum obcaecati, reprehenderit rem optio adipisci soluta autem. Consequatur cum, facere esse inventore laboriosam saepe dignissimos iste dolor architecto error aut officiis!
-      </Tweet>
-      <Tweet username={user.username} name={user.name}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum obcaecati, reprehenderit rem optio adipisci soluta autem. Consequatur cum, facere esse inventore laboriosam saepe dignissimos iste dolor architecto error aut officiis!
-      </Tweet>
-      <Tweet username={user.username} name={user.name}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum obcaecati, reprehenderit rem optio adipisci soluta autem. Consequatur cum, facere esse inventore laboriosam saepe dignissimos iste dolor architecto error aut officiis!
-      </Tweet>
-      <Tweet username={user.username} name={user.name}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum obcaecati, reprehenderit rem optio adipisci soluta autem. Consequatur cum, facere esse inventore laboriosam saepe dignissimos iste dolor architecto error aut officiis!
-      </Tweet>
-      <Tweet username={user.username} name={user.name}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum obcaecati, reprehenderit rem optio adipisci soluta autem. Consequatur cum, facere esse inventore laboriosam saepe dignissimos iste dolor architecto error aut officiis!
-      </Tweet>
-      <Tweet username={user.username} name={user.name}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum obcaecati, reprehenderit rem optio adipisci soluta autem. Consequatur cum, facere esse inventore laboriosam saepe dignissimos iste dolor architecto error aut officiis!
-      </Tweet>
-      <Tweet username={user.username} name={user.name}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum obcaecati, reprehenderit rem optio adipisci soluta autem. Consequatur cum, facere esse inventore laboriosam saepe dignissimos iste dolor architecto error aut officiis!
-      </Tweet>
-      <Tweet username={user.username} name={user.name}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum obcaecati, reprehenderit rem optio adipisci soluta autem. Consequatur cum, facere esse inventore laboriosam saepe dignissimos iste dolor architecto error aut officiis!
-      </Tweet>
-      <Tweet username={user.username} name={user.name}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum obcaecati, reprehenderit rem optio adipisci soluta autem. Consequatur cum, facere esse inventore laboriosam saepe dignissimos iste dolor architecto error aut officiis!
-      </Tweet>
-      <Tweet username={user.username} name={user.name}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum obcaecati, reprehenderit rem optio adipisci soluta autem. Consequatur cum, facere esse inventore laboriosam saepe dignissimos iste dolor architecto error aut officiis!
-      </Tweet>
+    <PageWrapper
+      fixedContent={
+        <>
+          <UserName>{user.name}</UserName>
+          <TweetContainer>
+            <img
+              src={`https://robohash.org/${user.username}`}
+              alt={user.name}
+            />
+            <TweetInput placeholder="O que está acontecendo" />
+          </TweetContainer>
+          <TweetButton>
+            <Button>Tweet</Button>
+          </TweetButton>
+        </>
+      }
+    >
+      {tweets.map((tweet) => (
+        <Tweet username={tweet.user.username} name={tweet.user.name} key={tweet.id}>
+          {tweet.content}
+
+        </Tweet>
+      ))}
     </PageWrapper>
-  )
+  );
 }
 
 export default Home;
