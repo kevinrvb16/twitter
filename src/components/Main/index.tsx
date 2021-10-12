d5c613c13208337175f3330c57ab4322b5632409
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-10-11 23:30:15 -0300

    feat: Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,16 @@
-const Main: React.FC = () => {
-    return <h1>Oiii</h1>
+import { Container, FixedContent, ScrollableContent } from "./styles"
+
+interface IProps {
+    fixedContent?: JSX.Element
+}
+
+const Main: React.FC<IProps> = ({ children, fixedContent }) => {
+    return (
+        <Container>
+            <FixedContent>{fixedContent}</FixedContent>
+            <ScrollableContent>{children}</ScrollableContent>
+        </Container>
+    )
 }
 
 export default Main
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,3 +1,28 @@
 import styled from "styled-components"
 
-export const Container = styled.div``
+export const Container = styled.div`
+    width: 598px;
+    height: 100vh;
+    border-left: 1px solid ${props => props.theme.color.borderColor};
+    border-right: 1px solid  ${props => props.theme.color.borderColor};
+    overflow: auto;
+
+    &::-webkit-scrollbar {
+        width: 0px;
+        background: transparent;
+    }
+`
+
+export const FixedContent = styled.div`
+    position: sticky;
+    top: 0;
+    background-color: #000;
+    border-bottom: 1px solid  ${props => props.theme.color.borderColor};
+    padding-left: 20px;
+    padding-right: 20px;
+`
+
+export const ScrollableContent = styled.div`
+        padding-left: 20px;
+        padding-right: 20px;
+`
